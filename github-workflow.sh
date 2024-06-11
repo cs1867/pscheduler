73ac6eec4663d9d19518819fa6eb4413b15851d3
cs1867 <blackhatracing@gmail.com>
2024-06-11 15:05:56 -0400

    Update and rename github-workflow to github-workflow.sh

--- github-workflow.sh
+++ github-workflow.sh
@@ -8,13 +8,13 @@
            dnf config-manager --add-repo . --setopt=gpgcheck=0
            ls -al /etc/yum.repos.d/
            dnf install -y make
-           cd /tmp/owamp
+           cd /tmp/pscheduler
            ls -al
            dnf -y install git
            git clone https://github.com/perfsonar/unibuild.git
            cd unibuild
            make
-           cd /tmp/owamp
+           cd /tmp/pscheduler
            ls -al /etc/yum.repos.d
            echo gpgcheck=0 >> /etc/yum.repos.d/tmp_artifacts_RPMS.repo
            cat /etc/yum.repos.d/tmp_artifacts_RPMS.repo
